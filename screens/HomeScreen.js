873dd1485d50ea155aea8b2dbd00fae0b09446ec
Waifu Forever <itachiut1r4@gmail.com>
2022-04-19 19:10:13 -0300

    add: HomeScreen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import { Image, Text, SafeAreaView, View } from "react-native";
+import tw from "tailwind-react-native-classnames";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{ width: 100, height: 100, resizeMode: "contain" }}
+          source={{ uri: "https://links.papareact.com/gzs" }}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
