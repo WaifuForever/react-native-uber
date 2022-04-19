2a919b50a117242356c6349c269f1935ba26e28d
Waifu Forever <itachiut1r4@gmail.com>
2022-04-19 19:47:06 -0300

    add: NavOptions

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
 import HomeScreen from "./src/screens/HomeScreen";
 import { store } from "./src/store";
@@ -6,7 +7,9 @@ import { store } from "./src/store";
 export default function App() {
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>
     </Provider>
   );
 }
