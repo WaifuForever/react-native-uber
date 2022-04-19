551de43f6e75ff2a76874a54a58b9f9ddf8dc9ff
Waifu Forever <itachiut1r4@gmail.com>
2022-04-19 19:20:59 -0300

    update: src folder

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { Provider } from "react-redux";
-import HomeScreen from "./screens/HomeScreen";
-import { store } from "./store";
+import HomeScreen from "./src/screens/HomeScreen";
+import { store } from "./src/store";
 
 export default function App() {
   return (
@@ -10,12 +10,3 @@ export default function App() {
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
