0e698f9d6079842202303c6bca6163827424e451
Waifu Forever <itachiut1r4@gmail.com>
2022-04-20 07:17:52 -0300

    add: GooglePlacesAutocomplete

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,15 @@
 import { Image, Text, SafeAreaView, View } from "react-native";
 import tw from "tailwind-react-native-classnames";
+import { useDispatch } from "react-redux";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import NavOptions from "../components/NavOptions";
+import { setDestination, setOrigin } from "../slices/navSlice";
+
+import { GOOGLE_MAPS_KEY } from "@env";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -10,6 +17,36 @@ const HomeScreen = () => {
           style={{ width: 100, height: 100, resizeMode: "contain" }}
           source={{ uri: "https://links.papareact.com/gzs" }}
         />
+
+        <GooglePlacesAutocomplete
+          placeholder="Where From?"
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+            dispatch(setDestination(null));
+          }}
+          query={{
+            key: GOOGLE_MAPS_KEY,
+            language: "en",
+          }}
+          enablePoweredByContainer={false}
+          fetchDetails={true}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+        />
+
         <NavOptions />
       </View>
     </SafeAreaView>
